2efe561a75c9685ae6f937ed11b19bda6dcf0a72
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-03-23 18:19:27 +0530

    Finalize the Customer-UI

---
+++ src/main/java/lk/ijse/dep10/customer/controller/CustomerSceneController.java
@@ -0,0 +1,39 @@
+package lk.ijse.dep10.customer.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class CustomerSceneController {
+     public Button btnDelete;
+     public Button btnNew;
+     public Button btnSave;
+     public TableView<?> tblCustomers;
+     public TextField txtAddress;
+     public TextField txtId;
+     public TextField txtName;
+
+     
+     public void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+     
+     public void btnNewOnAction(ActionEvent event) {
+
+    }
+
+     
+     public void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+     
+     public void tblCustomersOnKeyRelease(KeyEvent event) {
+
+    }
+
+}
