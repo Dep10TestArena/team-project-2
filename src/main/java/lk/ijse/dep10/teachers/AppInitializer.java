f2f2efaf97aefc9231cc6b9338cb7b1a9d58bce0
Anupama Karunarathne <anukar220@gmail.com>
2023-03-23 18:41:53 +0530

    Singleton applied for DBConnection

--- src/main/java/lk/ijse/dep10/teachers/AppInitializer.java
+++ src/main/java/lk/ijse/dep10/teachers/AppInitializer.java
@@ -1,7 +1,11 @@
 package lk.ijse.dep10.teachers;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -10,7 +14,13 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/TeacherView.fxml"))));
+
+        primaryStage.setTitle("Teacher Manager");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+        primaryStage.sizeToScene();
 
     }
 }
--- src/main/java/lk/ijse/dep10/teachers/db/DBConnection.java
+++ src/main/java/lk/ijse/dep10/teachers/db/DBConnection.java
@@ -1,2 +1,48 @@
-package lk.ijse.dep10.teachers.db;public class DBConnection {
+package lk.ijse.dep10.teachers.db;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private final Connection connection;
+
+
+
+    private DBConnection() {
+        File file = new File("application.properties");
+        try {
+            FileReader fileReader = new FileReader(file);
+            Properties properties = new Properties();
+            properties.load(fileReader);
+            fileReader.close();
+
+            String host = properties.getProperty("mysql.host", "localhost");
+            String port = properties.getProperty("mysql.port", "3306");
+            String database = properties.getProperty("mysql.database", "dep10_git");
+            String username = properties.getProperty("mysql.username", "root");
+            String password = properties.getProperty("mysql.password", "AnKar220#");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "createDatabaseIfNotExist=true";
+
+            connection = DriverManager.getConnection(url, username, password);
+
+
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static DBConnection getInstance() {
+        return (dbConnection==null)? dbConnection = new DBConnection():dbConnection;
+    }
+
+    public Connection getConnection() {return connection;}
 }
