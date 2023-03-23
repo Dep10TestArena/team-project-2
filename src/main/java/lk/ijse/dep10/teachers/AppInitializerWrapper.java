8b821261edf05d40c93126d49d7a846d91055584
Anupama Karunarathne <anukar220@gmail.com>
2023-03-23 18:21:38 +0530

    Completed the Manage Teacher UI

---
+++ src/main/java/lk/ijse/dep10/teachers/AppInitializerWrapper.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep10.teachers;
+
+public class AppInitializerWrapper {
+}
---
+++ src/main/java/lk/ijse/dep10/teachers/controller/TeacherViewController.java
@@ -0,0 +1,51 @@
+package lk.ijse.dep10.teachers.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class TeacherViewController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewTeacher;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Label lblHeading;
+
+    @FXML
+    private TableView<?> tblTeachers;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewTeacherOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
