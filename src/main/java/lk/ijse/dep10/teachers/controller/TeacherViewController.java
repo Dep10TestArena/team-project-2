ba8a9bc12fc9d1536ebe9b1c18b892c0fc4a2b65
Anupama Karunarathne <anukar220@gmail.com>
2023-03-23 19:10:33 +0530

    Created the Database

--- src/main/java/lk/ijse/dep10/teachers/controller/TeacherViewController.java
+++ src/main/java/lk/ijse/dep10/teachers/controller/TeacherViewController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class TeacherViewController {
 
@@ -32,6 +33,13 @@ public class TeacherViewController {
 
     @FXML
     private TextField txtName;
+
+    public void initialize() {
+        tblTeachers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblTeachers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblTeachers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+
+    }
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
